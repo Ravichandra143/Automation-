bd442af981fe5cbe720300138a2e857ea32391af
Ravi-Istar <Ravi-Istar@DESKTOP-RP66F0K>
2018-09-20 17:18:10 +0530

    This is the first commit

---
+++ Automation/src/test/java/tests/NewTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest {
+	
+	static WebDriver driver;
+  @Test
+  public void login() throws InterruptedException  {
+	  System.setProperty("webdriver.chrome.driver", ".\\exefiles\\geckodriver.exe");  
+	     
+      WebDriver driver = new FirefoxDriver();
+//      driver.manage().window().maximize();
+      driver.get("https://google.com");
+  }}
+      
+      
+	  
+	  
+  /*}
+  @Test(priority = 1 )
+  public void dropdownSelect() throws InterruptedException  {
+	  
+	  driver.findElement(By.id("exampleInputEmail1")).sendKeys("testing@istarindia.com");
+      driver.findElement(By.id("exampleInputPassword1")).sendKeys("test123");
+	  driver.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div/form/button")).click();
+	  Thread.sleep(2000);
+	  Assert.assertEquals(driver.getCurrentUrl(),"http://localhost:4200/#/dashboard");
+	  Thread.sleep(2000);
+	 
+	  
+	  
+	  
+	    }	
+
+  @AfterMethod
+  public void afterMethod() {
+	  driver.close();
+  }}
+*/
---
+++ Automation/src/test/java/tests/sample.java
@@ -0,0 +1,57 @@
+package tests;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class sample {  
+    private static String userName = "testing@istarindia.com" ;
+    private static String password = "test123" ;
+
+    public static void main(String[] args) throws IOException{
+       String chromeDriverPath = ".\\exefiles\\chromedriver.exe" ;
+       System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+       ChromeOptions options = new ChromeOptions();
+       options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080","--ignore-certificate-errors", "--silent");
+       WebDriver driver = new ChromeDriver(options);
+
+      // Get the login page
+      driver.get("https://sales.talentify.in/#/login");
+   
+      // Search for username / password input and fill the inputs
+      driver.findElement(By.id("exampleInputEmail1")).sendKeys(userName);
+      driver.findElement(By.id("exampleInputPassword1")).sendKeys(password);
+
+      // Locate the login button and click on it
+      driver.findElement(By.xpath("/html/body/app-root/app-login/div/div/div/div/form/button")).click();
+
+      if(driver.getCurrentUrl().equals("")){
+           System.out.println("Incorrect credentials");
+           driver.quit();
+           System.exit(1);
+      }else{
+           System.out.println("Successfuly logged in");
+      }
+      File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+      FileUtils.copyFile(screenshotFile, new File("C:\\screenshots"));
+        
+        // Logout
+        try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+        driver.findElement(By.xpath("//*[@id=\"dropdownMenu2\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"navbarCollapse\"]/div/div/div/button[2]")).click();
+      
+    driver.quit();
+   }
+}
